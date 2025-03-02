779c864c0664fcbb03e8d6e201b5c290d9bcc7ea
SyedZainulAbideen494 <zainkaleem27@gmail.com>
2025-03-02 14:46:30 +0530

    m

--- src/app modules/apiRoutes.js
+++ src/app modules/apiRoutes.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000'; //https://srv594954.hstgr.cloud
+const API_URL = 'https://g3zvcp57-5000.inc1.devtunnels.ms'; //https://srv594954.hstgr.cloud
 
 export const API_ROUTES = {
  createOrder: `${API_URL}/create-order`,
