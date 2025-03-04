8beb45058051718f5a61c69e4b443ec0834bd82c
SyedZainulAbideen494 <zainkaleem27@gmail.com>
2025-03-04 06:27:53 +0530

    m

--- src/ai chat bot/KisarChatbot.js
+++ src/ai chat bot/KisarChatbot.js
@@ -1,9 +1,10 @@
 import { useState, useEffect, useRef } from "react";
 import axios from "axios";
-import { FaAssistiveListeningSystems, FaComment } from "react-icons/fa";
+import { FaAssistiveListeningSystems, FaComment, FaTrashAlt } from "react-icons/fa";
 import "./KisarChatbot.css";
 import { API_ROUTES } from "../app modules/apiRoutes";
 import DOMPurify from "dompurify";
+import { FiTrash } from "react-icons/fi";
 
 const formatContent = (content) => {
   // Format code blocks
@@ -120,11 +121,20 @@ function KisarChatbot() {
     }
   };
 
+
+  const clearChat = () => {
+    const confirmClear = window.confirm("Are you sure you want to clear the chat?");
+    if (confirmClear) {
+      setChatHistory([{ role: "model", parts: [{ text: "Hey there! How can I assist you today?" }] }]);
+      localStorage.removeItem("kisar_chat");
+    }
+  };
+
+
   const SparkleIcon = () => (
     <svg height="24" width="24" fill="#FFFFFF" viewBox="0 0 24 24" data-name="Layer 1" id="Layer_1" class="sparkle">
     <path d="M10,21.236,6.755,14.745.264,11.5,6.755,8.255,10,1.764l3.245,6.491L19.736,11.5l-6.491,3.245ZM18,21l1.5,3L21,21l3-1.5L21,18l-1.5-3L18,18l-3,1.5ZM19.333,4.667,20.5,7l1.167-2.333L24,3.5,21.667,2.333,20.5,0,19.333,2.333,17,3.5Z"></path>
 </svg>
-
 );
 
   return (
